54b151d248fad76e16cd415091c7e542902cf66e
Amish sethi <amish.sethi@lessonbot.ai>
2020-08-24 23:04:46 -0400

    Made it so the controllers redirect to home instead of the donate masks form

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -29,7 +29,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = RouteServiceProvider::DONATE;
+    protected $redirectTo = RouteServiceProvider::HOME;
 
     /**
      * Create a new controller instance.
